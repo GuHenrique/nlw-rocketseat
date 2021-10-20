f14fc8bf9402beaf937087220687874e39676abe
Gustavo Henrique <gustavo._henrique@hotmail.com>
2021-10-19 23:34:10 -0300

    criação de estrutura dos componentes, e adicionados no App.tsx, instalado Sass e React-icons, Componente LoginBox layout terminado

---
+++ heat/web/src/components/LoginBox/index.tsx
@@ -0,0 +1,13 @@
+import styles from './styles.module.scss'
+import {VscGithubInverted} from 'react-icons/vsc'
+export function LoginBox(){
+  return (
+   <div className={styles.loginBoxWrapper}>
+     <strong>Entre e compartilhe sua mensagem</strong>
+     <a href="#" className={styles.signWithGithubButton}>
+       <VscGithubInverted size="24" />
+       Entrar com Github
+     </a>
+   </div>
+  )
+}
