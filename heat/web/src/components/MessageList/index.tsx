1470f1ac98968013193e8855f0c5b3b794f91a48
Gustavo Henrique <gustavo._henrique@hotmail.com>
2021-10-19 23:55:25 -0300

    layout componente MessageList Terminado - instalacao axios

--- heat/web/src/components/MessageList/index.tsx
+++ heat/web/src/components/MessageList/index.tsx
@@ -1,5 +1,50 @@
+import styles from './styles.module.scss'
+
+import logoImg from '../../assets/logo.svg'
+
 export function MessageList(){
   return (
-    <h1>MessageList</h1>
-  )
+    <div className={styles.messageListWrapper}>
+      <img className={styles.logo} src={logoImg} alt="DoWhile 2021  " />
+
+      <ul className={styles.messageList}>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat, ratione officia. Eaque harum perspiciatis aliquid!</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/GuHenrique.png" alt="Gustavo Henrique" />
+            </div>
+            <span>Gustavo Henrique</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat, ratione officia. Eaque harum perspiciatis aliquid!</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/GuHenrique.png" alt="Gustavo Henrique" />
+            </div>
+            <span>Gustavo Henrique</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat, ratione officia. Eaque harum perspiciatis aliquid!</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/GuHenrique.png" alt="Gustavo Henrique" />
+            </div>
+            <span>Gustavo Henrique</span>
+          </div>
+        </li>
+        <li className={styles.message}>
+          <p className={styles.messageContent}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat, ratione officia. Eaque harum perspiciatis aliquid!</p>
+          <div className={styles.messageUser}>
+            <div className={styles.userImage}>
+              <img src="https://github.com/GuHenrique.png" alt="Gustavo Henrique" />
+            </div>
+            <span>Gustavo Henrique</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+    )
 }
